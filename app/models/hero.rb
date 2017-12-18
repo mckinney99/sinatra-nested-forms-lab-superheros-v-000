694709a367f71584dc481ab29629c135582be3e2
Eric McKinney <mckinney99@gmail.com>
2017-12-18 04:23:37 +0000

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,21 @@
+class Hero
+  attr_accessor :name, :power, :biography
+
+  HEROES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @biography = args[:biography]
+
+    HEROES << self
+  end
+
+  def self.all
+    HEROES
+  end
+
+  def self.clear
+    self.all.clear
+  end
+end
